a995368ba06c34b74bda203f2085b62e7da59bd3
Gustavo Pares <gustavoanalytics@gmail.com>
2018-03-18 00:17:59 -0500

    finalize

--- main.rb
+++ main.rb
@@ -14,86 +14,14 @@ get '/about' do
   haml :about
 end
 
-get '/reservoir-management' do
-  haml :consult_1
-end
-
-get '/reservoir-characterization' do
-  haml :consult_2
+get '/jobs' do
+  haml :jobs
 end
 
 
-get '/multiphase-flow-systems' do
-  haml :consult_3
+get '/jobs-show' do
+  haml :jobs_show
 end
-
-
-get '/production-optimization' do
-  haml :consult_4
-end
-
-get '/software-development' do
-  haml :consult_5
-end
-
-get '/compact-separation' do
-  haml :training_1
-end
-
-get '/multiphase-flow-assurance' do
-  haml :training_2
-end
-
-get '/uncertainty-analysis' do
-  haml :training_3
-end
-
-get '/water-treatment' do
-  haml :training_4
-end
-
-get '/integrated-reservoir-development' do
-  haml :training_5
-end
-
-get '/inline-compact-water-separator' do
-  haml :equipment_1
-end
-
-get '/jet-gas' do
-  haml :equipment_2
-end
-
-get '/gas-liquid-cylindrical-cyclone-compact-separator' do
-  haml :equipment_3
-end
-
-
-get '/water-polisher' do
-  haml :equipment_4
-end
-
-get '/wellbore-sand-transport-simulator' do
-  haml :software_1
-end
-
-get '/uncertainty-based-production-allocation-analysis' do
-  haml :software_2
-end
-
-get '/iloop' do
-  haml :software_3
-end
-
-get '/custom-multiphase' do
-  haml :software_4
-end
-
-get '/careers' do
-  haml :careers
-end
-
-
 get '/contact' do
   @success_message = session[:success_message]
   session[:success_message] = nil
